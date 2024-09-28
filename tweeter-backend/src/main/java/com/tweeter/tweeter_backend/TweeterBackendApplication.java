ed5fb649f2681eed5400af1149fd768c67006c48
0xDVC <neilohene@gmail.com>
2024-09-28 02:00:44 +0000

    init: setting up from ground

---
+++ tweeter-backend/src/main/java/com/tweeter/tweeter_backend/TweeterBackendApplication.java
@@ -0,0 +1,13 @@
+package com.tweeter.tweeter_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TweeterBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TweeterBackendApplication.class, args);
+	}
+
+}

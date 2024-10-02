7bed53b51dc7633e8b4f378156b0dc77a4407466
0xDVC <neilohene@gmail.com>
2024-10-02 21:25:24 +0000

    chore: resolve dependency issue

--- tweeter-backend/src/main/java/com/tweeter/tweeter_backend/controllers/AuthenticationController.java
+++ tweeter-backend/src/main/java/com/tweeter/tweeter_backend/controllers/AuthenticationController.java
@@ -9,7 +9,6 @@ import com.tweeter.tweeter_backend.models.RegistrationObject;
 import com.tweeter.tweeter_backend.repositories.RoleRepository;
 import com.tweeter.tweeter_backend.repositories.UserRepository;
 import com.tweeter.tweeter_backend.service.UserService;
-import org.apache.tomcat.util.http.parser.HttpParser;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
